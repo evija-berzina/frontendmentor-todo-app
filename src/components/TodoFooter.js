548340e6945668dcbb0597ef8765bc123ca2ab55
eberzina <berzina3633@gmail.com>
2026-05-13 12:58:28 +0300

    Add hover style for todo footer

--- src/components/TodoFooter.js
+++ src/components/TodoFooter.js
@@ -1,9 +1,9 @@
 export default function TodoFooter({ isDarkMode, deleteCheckedTodos, filteredTodos  }) {
 
   return (
-    <div className={`flex flex-row justify-between items-center w-full bg-[hsl(var(--gray-50))] px-4 py-3 rounded-b-md ${isDarkMode ? 'bg-[hsl(var(--navy-900))]' : ''}`}>
+    <div className={`flex flex-row justify-between items-center w-full bg-[hsl(var(--gray-50))] px-4 py-3 rounded-b-md text-[hsl(var(--gray-600))] ${isDarkMode ? 'bg-[hsl(var(--navy-900))]' : ''}`}>
       <p className="">{filteredTodos.length} items left</p>
-      <button onClick={deleteCheckedTodos} className="cursor-pointer">Clear Completed</button>
+      <button onClick={deleteCheckedTodos} className={`cursor-pointer transition-colors duration-200 ${isDarkMode ? 'hover:text-[hsl(var(--gray-50))]' : 'hover:text-[hsl(var(--navy-900))]'}`}>Clear Completed</button>
     </div>
   );
 }
