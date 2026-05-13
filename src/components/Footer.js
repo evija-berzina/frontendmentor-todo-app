651e6230aa73c87106035a2bc175280530bf1d8f
eberzina <berzina3633@gmail.com>
2026-05-13 12:57:51 +0300

    Change text color on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-center">
+    <footer className="text-center text-[hsl(var(--gray-600))]">
       <p>Drag and drop to reorder list</p>
     </footer>
   );
