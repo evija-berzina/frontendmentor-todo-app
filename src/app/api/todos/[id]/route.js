653421558abb7b057d0dd52df17e8403ffcdb794
eberzina <berzina3633@gmail.com>
2026-05-14 16:32:49 +0300

    Add DELETE and PATCH API routes for todos

---
+++ src/app/api/todos/[id]/route.js
@@ -0,0 +1,38 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function DELETE(req, { params }) {
+  try {
+    const { id } = await params;
+
+    console.log("DELETE ID:", id);
+
+    const deleted = await prisma.todo.delete({
+      where: { id },
+    });
+
+    return NextResponse.json(deleted);
+  } catch (err) {
+    console.error("DELETE ERROR:", err);
+
+    return NextResponse.json(
+      { error: err.message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req, { params }) {
+  const { id } = await params;
+
+  const body = await req.json();
+
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: {
+      checked: body.checked,
+    },
+  });
+
+  return NextResponse.json(updatedTodo);
+}
